0f182657d34af8cb9de04dd6c5e536dfca286eaa
calico-cl <floofo@email.com>
2022-03-16 19:18:42 +0000

    v1

--- KitKyatto/GitHub Client/async stuff.cs
+++ KitKyatto/GitHub Client/async stuff.cs
@@ -14,8 +14,11 @@ namespace KitKyatto
         {
             Console.ForegroundColor = ConsoleColor.White;
             var client = new GitHubClient(new ProductHeaderValue("uwu"));
-            var creds = new Credentials("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");
+            var creds = new Credentials("enter here uwu");
             client.Credentials = creds;
+
+
+
             Console.Clear();
             Console.WriteLine("Enter The Username You Want To Search");
             Console.ForegroundColor = ConsoleColor.DarkCyan;
@@ -23,10 +26,17 @@ namespace KitKyatto
             var user = await client.User.Get(search);
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("{0} has {1} public repositories", search, user.PublicRepos);
+
             Console.WriteLine("\n\n" + search + " Has " + user.Followers + " Followers");
+
             Console.WriteLine("\n\n" + search + " Is Following " + user.Following + " Users");
+
             Console.WriteLine("\n\n" + search + "'s Bio: " + user.Bio);
+
             Console.WriteLine("\n\n" + search + "'s Account was created at " + user.CreatedAt.DateTime);
+
+
+
             Console.ForegroundColor = ConsoleColor.White;
         }
     }
