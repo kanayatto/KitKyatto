cbb5d6c4e682246bc40a784e26d190503026ed7e
Kanayatto <floofo@email.com>
2022-03-16 20:10:59 +0000

    Added Discord Commands (GitHub Account Statistics)

--- KitKyatto/Discord/Discord Client Stuffs.cs
+++ KitKyatto/Discord/Discord Client Stuffs.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Logging;
+using Discord.Net.Converters;
+using Discord.Rest;
+using Discord.WebSocket;
 
 namespace KitKyatto
 {
     internal class Discord_Client_Stuffs
     {
+        private DiscordSocketClient client;
+        public async Task Main()
+        {
+            client = new DiscordSocketClient();
+            client.MessageReceived += Commands;
 
+            string token = "token";
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public Task Commands(SocketMessage message)
+        {
+            if (message.Content == "uwu")
+            {
+                string extra = message.Content.Split(new[] {"uwu"}, StringSplitOptions.None)[1];
+                var avatar = message.Author.GetAvatarUrl();
+                EmbedBuilder uwu = new EmbedBuilder();
+                uwu.Title = "GitHub Statistics";
+                uwu.ImageUrl = @"https://github-readme-stats.vercel.app/api?username=" + extra + "&show_icons=true&theme=nord";
+            }
+
+
+
+            return Task.CompletedTask;
+        }
     }
 }
