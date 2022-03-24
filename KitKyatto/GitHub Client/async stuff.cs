437ba7e7b3b48783215dfd4cc6d3d742754f0fba
Kyatto <71313658+kanayatto@users.noreply.github.com>
2022-03-24 00:08:44 +0000

    fixed async stuff.cs

--- KitKyatto/GitHub Client/async stuff.cs
+++ KitKyatto/GitHub Client/async stuff.cs
@@ -34,8 +34,6 @@ namespace KitKyatto
             Console.WriteLine("\n\n" + search + "'s Bio: " + user.Bio);
 
             Console.WriteLine("\n\n" + search + "'s Account was created at " + user.CreatedAt.DateTime);
-            
-            Console.WriteLine("\n\n" + search " is very weird and annoying
 
 
             Console.ForegroundColor = ConsoleColor.White;
